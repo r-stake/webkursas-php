d780d0787a21a938539af6fb9c5b8bbcb723c7f4
r-stake <96791353+r-stake@users.noreply.github.com>
2023-05-31 18:00:02 +0300

    Journey section2 carousel

--- final-project/index.php
+++ final-project/index.php
@@ -161,8 +161,30 @@
                                     <div class="dots-corner">
                                         <img src="./images/dots-corner.png" alt="dots">
                                     </div>
-                                    
-                                    <img src="./images/vietove_2.jpg" alt="Labanoro giria">
+                                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img src="./images/forest_1.jpg" class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="./images/forest_2.jpg" class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="./images/forest_3.jpg" class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="./images/forest_4.jpg" class="d-block w-100" alt="...">
+                                            </div>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
+                                    </div>
                                 </div>
                                 <div class="journey-description">
                                     <h2>Pasiklysk girioje</h2>
